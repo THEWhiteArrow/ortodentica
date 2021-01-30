eb7089c5f712de4662011356b29c961e03ce6cca
WhiteArro <damitrafi6@gmail.com>
2021-01-30 21:03:42 +0100

    added  maps, imgs, seo++, custom cursor

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,5 @@
 const navbar = document.querySelector('#navbar');
+const cursor = document.querySelector('#cursor');
 const members = document.querySelectorAll('.member');
 const shownMember = {
    name: document.querySelector('#shownMemberName'),
@@ -9,20 +10,21 @@ const shownMember = {
 
 
 const init = () => {
+   setUpMembersTiles();
    setUpShownMember();
-   setUpMembersListener();
    setUpScrollListener();
+   setUpCursor();
 }
 
 const setUpShownMember = () => {
-   shownMember.pic.style.backgroundImage = members[3].style.backgroundImage;
-   shownMember.name.innerText = data[`member${1}`].name;
-   shownMember.info.innerText = data[`member${1}`].info;
+   shownMember.pic.style.backgroundImage = members[8].style.backgroundImage;
+   shownMember.name.innerText = data[`member${9}`].name;
+   shownMember.info.innerText = data[`member${9}`].info;
 }
 
-const setUpMembersListener = () => {
+const setUpMembersTiles = () => {
    members.forEach((member, i) => {
-      member.style.backgroundImage = `url("./public/assets/team/${i + 1}.jpg")`;
+      member.style.backgroundImage = `url("./public/assets/team/${i + 1}-min.jpg")`;
       member.addEventListener('click', () => {
 
          shownMember.pic.style.backgroundImage = member.style.backgroundImage;
@@ -37,20 +39,42 @@ const setUpMembersListener = () => {
 }
 
 const setUpScrollListener = () => {
-   window.addEventListener("scroll", () => {
+   document.addEventListener("scroll", () => {
       const scroll = this.scrollY;
       const viewHeight = this.innerHeight;
 
-      if ((scroll >= viewHeight || scroll >= 700) && !navbar.classList.contains('solid-nav')) {
+      if ((scroll >= viewHeight && scroll >= 700) && !navbar.classList.contains('solid-nav')) {
          navbar.classList.add('solid-nav');
          console.log('added solid-nav')
-      } else if ((scroll < viewHeight || scroll < 700) && navbar.classList.contains('solid-nav')) {
+      } else if ((scroll < viewHeight && scroll < 700) && navbar.classList.contains('solid-nav')) {
          navbar.classList.remove('solid-nav');
          console.log('removed solid-nav')
       }
    });
 }
 
+const setUpCursor = () => {
+   if (!isMobile()) {
+      document.addEventListener('mousemove', () => {
+         let e = window.event;
+
+         cursor.style.setProperty('--mtop', e.clientY - cursor.offsetHeight / 2 + 'px');
+         cursor.style.setProperty('--mleft', e.clientX - cursor.offsetWidth / 2 + 'px');
+      });
+
+      document.addEventListener('mousedown', () => {
+         cursor.classList.add('mouse-down');
+      });
+
+      document.addEventListener('mouseup', () => {
+         cursor.classList.remove('mouse-down');
+      });
+   }
+}
+
+const isMobile = () => {
+   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
 
 
