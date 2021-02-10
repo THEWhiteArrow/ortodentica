7c39b1f71c52ca1bce4deb29a49f88d7e3b35d24
WhiteArro <damitrafi6@gmail.com>
2021-02-10 21:44:17 +0100

    improved performance

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -161,6 +161,8 @@ const isMobile = () => {
    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-
+const easyMode = () => {
+   Scrollbar.destroyAll()
+}
 
 init();
