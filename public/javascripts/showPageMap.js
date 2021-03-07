5cc848354ae23c1af917d127b3c9fd08d1678ae3
WhiteArro <damitrafi6@gmail.com>
2021-03-07 18:45:12 +0100

    added api mapbox

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,26 @@
+mapboxgl.accessToken = mapToken;
+
+
+const map = new mapboxgl.Map({
+   container: 'map',
+   style: 'mapbox://styles/mapbox/outdoors-v11', // stylesheet location
+   center: [21.2132904, 52.1622951],
+   zoom: 17 // starting zoom
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+new mapboxgl.Marker()
+   .setLngLat([21.2132904, 52.1622951])
+   // .setPopup(
+   //    new mapboxgl.Popup({ offset: 25 })
+   //       .setHTML(
+   //          `<h3>OrtoDentica Stomatologia</h3>`
+   //       )
+   // )
+   .addTo(map)
+
+new mapboxgl.Popup({ closeOnClick: false, closeButton: false, closeOnMove: false, focusAfterOpen: false, offset: 35, maxWisth: 'none' })
+   .setLngLat([21.2132904, 52.1622951])
+   .setHTML('<h3>Stomatologia Ortodentica</h3> <p> - Kędzierzyńska 9, Warszawa</p>')
+   .addTo(map);
