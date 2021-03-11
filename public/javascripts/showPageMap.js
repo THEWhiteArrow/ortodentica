c05ff41b66a0d409326fa889c3aa8e5cf10c683b
WhiteArro <damitrafi6@gmail.com>
2021-03-11 12:30:49 +0100

    fixed map pop'up

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -8,19 +8,21 @@ const map = new mapboxgl.Map({
    zoom: 17 // starting zoom
 });
 
-map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
    .setLngLat([21.2132904, 52.1622951])
    // .setPopup(
-   //    new mapboxgl.Popup({ offset: 25 })
+   //    new mapboxgl.Popup({ closeOnClick: false, closeButton: false, closeOnMove: false, focusAfterOpen: true, offset: 35, maxWidth: 'none' })
    //       .setHTML(
-   //          `<h3>OrtoDentica Stomatologia</h3>`
+   //          `<h3>Stomatologia Ortodentica</h3> <p> - Kędzierzyńska 9, Warszawa</p>`
    //       )
    // )
    .addTo(map)
 
-new mapboxgl.Popup({ closeOnClick: false, closeButton: false, closeOnMove: false, focusAfterOpen: false, offset: 35, maxWisth: 'none' })
+new mapboxgl.Popup({ closeOnClick: false, closeButton: false, closeOnMove: false, focusAfterOpen: false, offset: 35, maxWidth: 'none' })
    .setLngLat([21.2132904, 52.1622951])
    .setHTML('<h3>Stomatologia Ortodentica</h3> <p> - Kędzierzyńska 9, Warszawa</p>')
    .addTo(map);
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+map.scrollZoom.disable();
